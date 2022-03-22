1270c36d4f075ceb603e6e2315028611eb45007a
maanan <maanan.said@gmail.com>
2022-03-22 01:17:43 +0100

    Change date in graphs.

--- analysis.R
+++ analysis.R
@@ -13,11 +13,19 @@ gpclibPermit()
 
 # Plot birth year distribution of NYC Citibike Users
 
+citibank$birth.year = as.integer(citibank$birth.year)
+
+citibank$usertype = as.factor(citibank$usertype)
+
+citibank$gender = as.factor(citibank$gender)
+
+levels(citibank$gender) = c("UNKNOWN", "MALE", "FEMALE")
+
 x.min <- quantile(citibank$birth.year, 0.001)
 
 ggplot(citibank) + geom_histogram(aes(birth.year), color = "darkorange",  fill="cornsilk", alpha=0.4) +
   geom_vline(xintercept = 1988, linetype = "dashed", color = "darkgreen") +
-  labs(title = "Birth Year of NYC Citibike Users", subtitle = "Data from 01/01/2018 to 31/12/2019") +
+  labs(title = "Birth Year of NYC Citibike Users", subtitle = "Data from 01/01/2017 to 31/12/2018") +
   xlab("Birth Year") + ylab("Trips") +
   xlim(c(x.min, 2002)) + theme_bw()
 
@@ -27,7 +35,7 @@ ggsave(path = "Graphs", filename = "birth_year_histogram.png")
 
 ggplot(citibank) + 
   geom_bar(aes(usertype, y = (..count..)/sum(..count..)), color = "darkorange",  fill="cornsilk", alpha=0.4, width=0.3) + ylab("Proportion") +
-  labs(title = "Share of Subscribers and Customers among NYC Citibike Users", subtitle = "Data from 01/01/2018 to 31/12/2019") +
+  labs(title = "Share of Subscribers and Customers among NYC Citibike Users", subtitle = "Data from 01/01/2017 to 31/12/2018") +
   xlab("User Type") + ylab("Proportion") +
   theme_bw()
 
@@ -37,7 +45,7 @@ ggsave(path = "Graphs", filename = "user_type.png")
 
 ggplot(citibank) + 
   geom_bar(aes(gender, y = (..count..)/sum(..count..)), color = "darkorange",  fill="cornsilk", alpha=0.4, width=0.3) + ylab("Proportion") +
-  labs(title = "Gender Distribution of NYC Citibike Users", subtitle = "Data from 01/01/2018 to 31/12/2019") +
+  labs(title = "Gender Distribution of NYC Citibike Users", subtitle = "Data from 01/01/2017 to 31/12/2018") +
   xlab("Gender") + ylab("Proportion") +
   theme_bw()
 
@@ -49,7 +57,7 @@ citibank %>%
   filter(gender != "UNKNOWN") %>% 
   ggplot(.)  + 
   geom_density(aes(birth.year,group=gender, fill=gender, colour=gender), adjust=3, alpha=0.1) +
-  labs(title = "Age Distribution of Male and Female Citibike Users", subtitle = "Data from 01/01/2018 to 31/12/2019") +
+  labs(title = "Age Distribution of Male and Female Citibike Users", subtitle = "Data from 01/01/2017 to 31/12/2018") +
   scale_fill_brewer(palette="Dark2") + scale_color_brewer(palette="Dark2") +
   xlab("Birth Year") + ylab("Density") + xlim(c(x.min, 2002)) +
   theme_bw()
@@ -63,7 +71,7 @@ citibank  <- mutate(citibank, tripduration.min = tripduration/60)
 x.max <- quantile(citibank$tripduration.min, 0.99)
 
 ggplot(citibank) + geom_histogram(aes(tripduration.min), color = "darkorange",  fill="cornsilk", alpha=0.4) +
-  labs(title = "Dsitribution of Trip Duration of NYC Citibike Users", subtitle = "Data from 01/01/2018 to 31/12/2019") +
+  labs(title = "Dsitribution of Trip Duration of NYC Citibike Users", subtitle = "Data from 01/01/2017 to 31/12/2018") +
   xlab("Trip Duration (in minutes)") + ylab("Number of Trips") +
   xlim(c(0, x.max)) + theme_bw()
 
@@ -77,7 +85,7 @@ citibank$hour    = as.factor(hour(citibank$starttime))
 
 ggplot(citibank) +
   geom_bar(aes(x=weekday, y=(..count..)/sum(..count..)), color = "darkorange",  fill="cornsilk", alpha=0.4) +
-  labs(title = "Percentage of Trips in Each Day of the Week", subtitle = "Data from 01/01/2018 to 31/12/2019") +
+  labs(title = "Percentage of Trips in Each Day of the Week", subtitle = "Data from 01/01/2017 to 31/12/2018") +
   xlab("Weekday") + ylab("") +
   theme_bw()
 
@@ -85,7 +93,7 @@ ggsave(path = "Graphs", filename = "weekday_use.png")
 
 ggplot(citibank) + 
   geom_bar(aes(x=weekday, y=(..count..)/sum(..count..), fill=usertype), position = position_stack(reverse = TRUE), color = "darkorange", alpha=0.4) +
-  labs(title = "Percentage of Daily Trips by User Type", subtitle = "Data from 01/01/2018 to 31/12/2019", fill = "User Type") +
+  labs(title = "Percentage of Daily Trips by User Type", subtitle = "Data from 01/01/2017 to 31/12/2018", fill = "User Type") +
   scale_fill_manual(values = c("darkorange3","cornsilk")) +
   xlab("Weekday") + ylab("") +
   theme_bw()
@@ -96,7 +104,7 @@ ggsave(path = "Graphs", filename = "weekday_user_type.png")
 
 ggplot(citibank) +
   geom_bar(aes(x=hour, y=(..count..)/sum(..count..)), color = "darkorange",  fill="cornsilk", alpha=0.4) +
-  labs(title = "Percentage of Trips in Each Hour of the Day", subtitle = "Data from 01/01/2018 to 31/12/2019") +
+  labs(title = "Percentage of Trips in Each Hour of the Day", subtitle = "Data from 01/01/2017 to 31/12/2018") +
   xlab("Hour") + ylab("") +
   theme_bw()
 
@@ -104,7 +112,7 @@ ggsave(path = "Graphs", filename = "hourly_use.png")
 
 ggplot(citibank) + 
   geom_bar(aes(x=hour, y=(..count..)/sum(..count..), fill=usertype), position = position_stack(reverse = TRUE), color = "darkorange", alpha=0.4) +
-  labs(title = "Percentage of Hourly Trips by User Type", subtitle = "Data from 01/01/2018 to 31/12/2019", fill = "User Type") +
+  labs(title = "Percentage of Hourly Trips by User Type", subtitle = "Data from 01/01/2017 to 31/12/2018", fill = "User Type") +
   scale_fill_manual(values = c("darkorange3","cornsilk")) +
   xlab("Hour") + ylab("") +
   theme_bw()
